2f3af4665e2df2df5152f7666514130f7cd8c5c1
José María Martín Sánchez <jmmarsan@gmail.com>
2019-03-08 10:53:47 +0100

    Initial commit

---
+++ WebAppWithSASS/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebAppWithSASS
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            //BundleTable.EnableOptimizations = true;
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
